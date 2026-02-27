f60195fa334e77e9bad43dd65fb89d6974243830
pratik49484-bit <pratik49484@gmail.com>
2026-02-27 19:11:58 +0530

    Configured AWS DB and secret key

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-kho-kho-production-key-123456"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
@@ -121,7 +121,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'khokho_db',
         'USER': 'postgres',
-        'PASSWORD': 'Mahakal123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
